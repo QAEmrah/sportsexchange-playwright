34fb10167b778e97716d13bbcb0e2c0603e760a4
Emrah Koksal <mikejohnson3505@gmail.com>
2026-01-06 11:39:50 +0300

    added retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,17 +3,20 @@ import { defineConfig } from '@playwright/test';
 import { env } from './src/utils/env';
 
 export default defineConfig({
+  retries: process.env.CI ? 2 : 0,
+
   reporter: [
     ['list'],
     ['junit', { outputFile: 'test-results/junit.xml' }],
     ['allure-playwright', { outputFolder: 'allure-results' }],
     ['html', { open: 'never' }],
   ],
+
   use: {
     baseURL: env.baseUrl,
     viewport: { width: 1280, height: 720 },
     headless: env.headless,
     trace: 'on-first-retry',
-    video: 'retain-on-failure', // or 'on' / 'off' / 'on-first-retry'
+    video: 'retain-on-failure',
   },
 });
